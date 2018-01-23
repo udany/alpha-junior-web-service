7c4798cbc567d7520f6f3f14e5dc4b671a661955
udany <long.eagle.goodies@gmail.com>
2018-01-23 17:16:07 -0200

    new routes for user

--- routes/users.js
+++ routes/users.js
@@ -5,28 +5,63 @@ import chalk from 'chalk'
 let router = express.Router();
 
 let users = [
-    {name: "Daniel", gender: "male"},
-    {name: "Vanessa", gender: "female"}
+    {name: "Daniel", gender: "male", hairColor: "Black"},
+    {name: "Vanessa", gender: "female", hairColor: "Dark Brown"}
 ];
 
-router.get('/add/', async function (req, res, next) {
-    let {name, gender = ''} = req.query;
+router.get('/save/', function (req, res, next) {
+    let {name, gender = '', hairColor=''} = req.query;
     if (name) {
-        users.push({
-            name, gender
-        });
+        let updatedUser = users.find(x => x.name === name);
+        if (updatedUser){
+            updatedUser.gender = gender.toLowerCase();
+            updatedUser.hairColor = hairColor;
+        }else{
+            users.push({
+                name, gender, hairColor
+            });
+        }
         res.send({status: "success"});
     } else {
-        res.send({status: "success"});
+        res.send({status: "failed"});
     }
 });
 
-router.get('/', async function (req, res, next) {
+
+
+
+router.get('/remove/', function (req, res, next){
+    let {name} = req.query;
+    if (name) {
+        let removedUser = users.find(x => x.name === name);
+        if(removedUser){
+            users.remove(removedUser);
+            res.send({status: "User removed"});
+        }
+        else{
+            res.send({status: "No user found"});
+        }
+    }
+});
+
+
+
+router.get('/', function (req, res, next) {
     let {gender = ''} = req.query;
     
     let filteredUsers = users.filter(x => !gender || x.gender === gender);
+    /*let filteredUsers = users.filter(function(x){
+        return !gender || x.gender === gender
+    });*/
+    
     
     res.send(filteredUsers);
+});
+           
+router.get('/test', async function (req, res, next)  {    
+    let {data} = await axios.get('https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49');
+    
+    res.send(data.title);
 });
 
 module.exports.path = '/user';
