f1836326628318daee24b5876be698f7aaa5333c
udany <long.eagle.goodies@gmail.com>
2018-01-24 17:20:22 -0200

    Persistência em arquivos

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,35 @@
 import express from 'express'
 import axios from 'axios'
 import chalk from 'chalk'
+import fileSystem from 'fs-extra'
+
+async function loadUsers(){
+    try {
+        let data = await fileSystem.readFile("users.json");
+        console.log("Funcionou o load");
+        users = JSON.parse(data);
+    }catch(error){
+        console.log(error);
+    }    
+}
+
+async function saveUsers(){
+    try {
+        await fileSystem.writeFile("users.json", JSON.stringify(users));
+        console.log("Funcionou");   
+    }catch(error) {
+        console.log(error);
+    }
+    
+    console.log(users);
+}
 
 let router = express.Router();
 
-let users = [
-    {name: "Daniel", gender: "male", hairColor: "Black"},
-    {name: "Vanessa", gender: "female", hairColor: "Dark Brown"}
-];
+let users = [];
+loadUsers();
 
+//add (edit) users
 router.get('/save/', function (req, res, next) {
     let {name, gender = '', hairColor=''} = req.query;
     if (name) {
@@ -21,13 +42,12 @@ router.get('/save/', function (req, res, next) {
                 name, gender, hairColor
             });
         }
+        saveUsers();
         res.send({status: "success"});
     } else {
         res.send({status: "failed"});
     }
 });
-
-
 
 
 router.get('/remove/', function (req, res, next){
@@ -36,6 +56,7 @@ router.get('/remove/', function (req, res, next){
         let removedUser = users.find(x => x.name === name);
         if(removedUser){
             users.remove(removedUser);
+            saveUsers();
             res.send({status: "User removed"});
         }
         else{
@@ -44,8 +65,6 @@ router.get('/remove/', function (req, res, next){
     }
 });
 
-
-
 router.get('/', function (req, res, next) {
     let {gender = ''} = req.query;
     
@@ -53,7 +72,6 @@ router.get('/', function (req, res, next) {
     /*let filteredUsers = users.filter(function(x){
         return !gender || x.gender === gender
     });*/
-    
     
     res.send(filteredUsers);
 });
